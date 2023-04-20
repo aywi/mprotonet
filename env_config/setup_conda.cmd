a2578d075136262928f176a4ecd21acd6ad7b2d9
Yuanyuan Wei <aywi@users.noreply.github.com>
2023-04-21 00:18:31 +0800

    Update setup scripts for Miniconda/Anaconda

--- env_config/setup_conda.cmd
+++ env_config/setup_conda.cmd
@@ -1,10 +1,10 @@
 @echo off
 call conda create -n mprotonet -y python=3.10
 call conda activate mprotonet
-call conda install -y pip numpy scipy matplotlib ipython jupyter
+call conda install -y numpy scipy scikit-learn matplotlib jupyter
 call conda install -c pytorch -c nvidia -y pytorch==1.13.1 torchvision==0.14.1 torchaudio==0.13.1 pytorch-cuda=11.6
 call conda install -c pytorch -y captum
-pip install -U torchio
+pip install -U opencv-contrib-python torchio
 call conda clean -a -y
 call conda info
 pause
--- env_config/setup_conda.sh
+++ env_config/setup_conda.sh
@@ -1,9 +1,9 @@
 conda create -n mprotonet -y python=3.10
 . $(conda info --base)/etc/profile.d/conda.sh
 conda activate mprotonet
-conda install -y pip numpy scipy matplotlib ipython jupyter
+conda install -y numpy scipy scikit-learn matplotlib jupyter
 conda install -c pytorch -c nvidia -y pytorch==1.13.1 torchvision==0.14.1 torchaudio==0.13.1 pytorch-cuda=11.6
 conda install -c pytorch -y captum
-pip install -U torchio
+pip install -U opencv-contrib-python torchio
 conda clean -a -y
 conda info
